2dc4f1191a8e2eff5d7693767a31b0ffee285c0b
MuppidiDeepthi <124231304+MuppidiDeepthi@users.noreply.github.com>
2023-12-29 14:50:23 +0530

    Create manage.py

---
+++ manage.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+st.sidebar.title("Menu")
+st.sidebar.subheader("Choose an option")
+st.title("Student Management")
+st.sidebar.title("Fee Management")
+st.sidebar.subheader("Add Student")
+rollnumber= st.sidebar.number_input("Rollnumber")
+name = st.sidebar.text_input("Name")
+fees = st.sidebar.number_input("Fees")
+add = st.sidebar.button("Add")
